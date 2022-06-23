a541f1353a293c709a582270e83bea689c8ce53a
pkhond <95232195+pkhond@users.noreply.github.com>
2022-06-23 16:18:29 +0100

    comment added for closing modal (#5)

--- pageobjects/locators.py
+++ pageobjects/locators.py
@@ -1,7 +1,9 @@
-#Credential Modal elements
+# Credential Modal elements
+# xpath for closing modal
 
-click_close= "xpath://button[@aria-label='Close']//*[name()='svg']//*[name()='path' and contains(@fill-rule,'evenodd')]"
-
-txt_loginEmail=    "xpath://input[@id='bink-form-field-credentials-email']"
-txt_loginPassword=  "xpath://input[@id='bink-form-field-credentials-password']"
-btn_credential=  "//div[normalize-space()='Verify Credentials']"
+click_close = "xpath://button[@aria-label='Close']//" \
+              "*[name()='svg']//*[name()='path' " \
+              "and contains(@fill-rule,'evenodd')]"
+txt_loginEmail = "xpath://input[@id='bink-form-field-credentials-email']"
+txt_loginPassword = "xpath://input[@id='bink-form-field-credentials-password']"
+btn_credential = "//div[normalize-space()='Verify Credentials']"
