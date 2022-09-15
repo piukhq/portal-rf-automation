a2334a2db500ba6949baa80a116853e9fe0e0297
navyaannajames <njames@bink.com>
2022-09-15 16:31:18 +0100

    commit asset comparator fix

--- portal_ui/pageobjects/locators.py
+++
@@ -1,9 +0,0 @@
-# Credential Modal elements
-# xpath for closing modal
-
-click_close = "xpath://button[@aria-label='Close']//" \
-              "*[name()='svg']//*[name()='path' " \
-              "and contains(@fill-rule,'evenodd')]"
-txt_loginEmail = "xpath://input[@id='bink-form-field-credentials-email']"
-txt_loginPassword = "xpath://input[@id='bink-form-field-credentials-password']"
-btn_credential = "//div[normalize-space()='Verify Credentials']"
